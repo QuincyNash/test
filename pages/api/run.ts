9ea8d28ce427dd631288d9d568dec885383ce51f
quincynash <quincy.nash@icloud.com>
2022-08-04 21:24:29 -0700

    Changes

--- pages/api/run.ts
+++ pages/api/run.ts
@@ -9,8 +9,13 @@ export default function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
+	const code = req.body as string;
+
+	const firstCommand = code.split(" ")[0];
+	const args = code.split(" ").slice(1);
+
 	return new Promise(async () => {
-		const ls = spawn("python", ["--version"]);
+		const ls = spawn(firstCommand, args);
 
 		ls.stdout.on("data", (data) => {
 			return res.status(200).json({ message: data.toString() });
