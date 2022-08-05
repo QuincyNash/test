b85d8bf36cf49fef6d0b4179e31ffa5779f0ca6d
quincynash <quincy.nash@icloud.com>
2022-08-04 23:20:17 -0700

    Changes

--- pages/api/run.ts
+++ pages/api/run.ts
@@ -1,5 +1,8 @@
 import { spawn } from "child_process";
 import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import crypto from "crypto";
+import { writeFileSync, unlinkSync } from "fs";
 
 type Data = {
 	message: string;
@@ -11,14 +14,32 @@ export default function handler(
 ) {
 	const code = req.body as string;
 
-	const firstCommand = code.split(" ")[0];
+	// const filePath = path.join(
+	// 	__dirname,
+	// 	"..",
+	// 	"..",
+	// 	`${crypto.randomBytes(16).toString("hex")}.py`
+	// );
+
+	// writeFileSync(filePath, code);
+
+	// console.log(filePath);
+
+	const command = code.split(" ")[0];
 	const args = code.split(" ").slice(1);
 
 	return new Promise(async () => {
-		const ls = spawn(firstCommand, args);
+		// const ls = spawn("python3", [filePath]);
+		const ls = spawn(command, args);
 
 		ls.stdout.on("data", (data) => {
+			console.log(data.toString());
 			return res.status(200).json({ message: data.toString() });
+		});
+
+		ls.on("exit", () => {
+			// unlinkSync(filePath);
+			console.log("DONE");
 		});
 	});
 }
