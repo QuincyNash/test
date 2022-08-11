4f7e84e1d40fb0a27ef8ca75ccd2882fc839036d
quincynash <quincy.nash@icloud.com>
2022-08-10 21:03:33 -0700

    Changes

--- pages/api/run.ts
+++ pages/api/run.ts
@@ -14,33 +14,33 @@ export default function handler(
 ) {
 	const code = req.body as string;
 
-	// const filePath = path.join(
-	// 	__dirname,
-	// 	"..",
-	// 	"..",
-	// 	`${crypto.randomBytes(16).toString("hex")}.py`
-	// );
+	const filePath = path.join(
+		__dirname,
+		"..",
+		"..",
+		`${crypto.randomBytes(16).toString("hex")}.py`
+	);
 
-	// writeFileSync(filePath, code);
+	writeFileSync(filePath, code);
 
 	// // console.log(filePath);
 
-	const split = code.split(" ");
-	const command = split[0] || "";
-	const args = split.length > 1 ? code.split(" ").slice(1) : undefined;
+	// const split = code.split(" ");
+	// const command = split[0] || "";
+	// const args = split.length > 1 ? code.split(" ").slice(1) : undefined;
 
 	return new Promise(async () => {
-		// const ls = spawn("python3", [filePath]);
-		const ls = spawn(command, args, {
-			shell: true,
-		});
+		const ls = spawn("python", [filePath]);
+		// const ls = spawn(command, args, {
+		// 	shell: true,
+		// });
 
 		ls.stdout.on("data", (data) => {
 			return res.status(200).json({ message: data.toString() });
 		});
 
 		ls.on("exit", () => {
-			// unlinkSync(filePath);
+			unlinkSync(filePath);
 		});
 	});
 }
